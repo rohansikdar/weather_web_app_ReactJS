8b290964c4c0d35976b54d6b84ce9130d03274e8
Rohan <rohansikdar97@gmail.com>
2023-04-23 14:44:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchMain from './components/searchMain';
+function App() {
+  return (
+    <div className="App">
+     <SearchMain />
+    </div>
+  );
+}
+
+export default App;
